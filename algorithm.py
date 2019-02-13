90f0623bd8ba26b36fb397b6868223d4bb45fae8
Totole75 <anatole.parre@eleves.enpc.fr>
2019-02-13 11:55:50 +0100

    predict and ridge regression

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,8 @@
-import tools
-import kernel
+import numpy as np
 
-folder_name = 'data'
-file_feat = 'Xtr1_mat100.csv'
-feat = tools.read_file(os.path.join(folder_name, file_feat))
-file_label = 'Ytr1.csv'
-label = tools.read_file(os.path.join(folder_name, file_label))
-
-K = kernel.gaussian_kernel(feat, 1)
+def ridge_regression(K, label_array, lambda_reg):
+    n = K.shape[0]
+    alpha = np.linalg.inv(K + lambda_reg*n*np.eye(n))
+    alpha = alpha.dot(label_array)
+    return(alpha)
+    
--- kernel.py
+++ kernel.py
@@ -10,14 +10,23 @@ class linear_kernel:
         self.kernel_array = np.dot(data_array, data_array.T)
     
 class gaussian_kernel:
-    
     def __init__(self, data_array, sigma):
         self.data_array = data_array
-        n = self.data_array.shape[0]
-        trnorms1 = np.mat([(np.dot(v,v.T)) for v in self.data_array]).T
-        k1 = trnorms1 * np.mat(np.ones((n, 1), dtype=np.float64)).T
-        k2 = np.mat(np.ones((n, 1), dtype=np.float64)) * trnorms1.T
-        k = k1 + k2 - 2* np.dot(self.data_array, self.data_array.T)
-        k *= - 1./(2 * np.power(sigma, 2))
+        self.sigma = sigma
+        self.n = self.data_array.shape[0]
+        self.kernel_array = self.compute_kernel(data_array, data_array)
         
-        self.kernel_array = np.exp(k)
+    def compute_kernel(self, data_array_1, data_array_2):
+        trnorms1 = np.mat([(np.dot(v,v.T)) for v in data_array_1]).T
+        trnorms2 = np.mat([(np.dot(v,v.T)) for v in data_array_2]).T
+        k1 = trnorms1 * np.mat(np.ones((data_array_2.shape[0], 1), dtype=np.float64)).T
+        k2 = np.mat(np.ones((self.n, 1), dtype=np.float64)) * trnorms2.T
+        k = k1 + k2 - 2* np.dot(data_array_1, data_array_2.T)
+        k *= - 1./(2 * np.power(self.sigma, 2))
+        kernel_array = np.exp(k)
+        return(kernel_array)
+        
+    def predict(self, test_array, alpha):
+        K = self.compute_kernel(self.data_array, test_array)
+        return(alpha.reshape(1,-1).dot(K))
+        
